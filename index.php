9de78e978c0ab9d2278292066c9b8e05721710c6
Unknown <nm.mutia02@gmail.com>
2018-05-15 00:29:36 +0700

    init

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Homepage</title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+	<div>
+		<nav class="navbar navbar-fixed-top navbar-expand-lg navbar-dark bg-dark">
+		 	<a class="navbar-brand" href="#">Rona</a>
+ 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    			<span class="navbar-toggler-icon"></span>
+  			</button>
+  			<div class="collapse navbar-collapse" id="navbarNavDropdown">
+			    <ul class="nav navbar-nav">
+			    	<li class="nav-item active">
+			        	<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			      	</li>
+			      	<li class="nav-item">
+			        	<a class="nav-link" href="#">Features</a>
+			      	</li>
+			      	<li class="nav-item">
+			        	<a class="nav-link" href="#">Pricing</a>
+			      	</li>
+			      	<li class="nav-item dropdown">
+			        	<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown link</a>
+				        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+				        	<a class="dropdown-item" href="#">Action</a>
+				          	<a class="dropdown-item" href="#">Another action</a>
+				          	<a class="dropdown-item" href="#">Something else here</a>
+				        </div>
+			      	</li>
+			    </ul>
+		  	</div>
+		  	<ul class="nav navbar-nav navbar-right">
+			    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+			   	<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+			</ul>
+		</nav>
+	</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
